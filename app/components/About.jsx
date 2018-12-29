48b57bfce2ba9e3b506e600eedad6b9d2f46ea2f
Caio Fascina <caio.fascina@tagcom.com.br>
2018-12-29 14:59:22 -0200

    Styles for About page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-    <h3>About</h3>
-    <p>Text for about page.</p>
+    <h1 className="text-center">About</h1>
+    <p>This is a weather application developed with React. It was developed within the course The Complete Rect Web App Developer Course.</p>
+    <p>Here are some tools that were used:</p>
+    <ul>
+      <li>
+        <a target="_blank" href="https://facebook.github.io/react/">React</a> - Javascript framework used.
+      </li>
+      <li>
+        <a target="_blank" href="http://openweathermap.org/">Open Weather Map</a> - API used for search for weather data by city name.
+      </li>
+    </ul>
   </div>
   );
 }
